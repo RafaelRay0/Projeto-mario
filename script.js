a4d64f2a06dd594a0216c256ffc2d0625091810f
RafaelRay0 <rr1010ray@gmail.com>
2024-02-15 11:10:52 -0300

    comitei

---
+++ script.js
@@ -0,0 +1,15 @@
+
+    let formulário = document.querySelector(".Fale-conosco")
+    let mascara = document.querySelector(".mascara-form")
+
+    function cliqueiNoBotao(){
+       formulário.style.left = "auto"
+       mascara.style.visibility = "visible"
+  }
+
+  function sumirFormulario(){
+        formulário.style.left = "-220px"
+        mascara.style.visibility = "hidden"
+  }
+
+
